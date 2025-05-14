ea04bfb1cc12573db1586548e6bc1a9bb79f0498
Carlos Eduardo Carfi <157553755+CaduCarfi@users.noreply.github.com>
2025-05-14 10:06:37 -0300

    Criei uma interação ao descer a pagina
    criação de animação com javascript para a pagina index.html

--- script.js
+++ script.js
@@ -99,3 +99,18 @@ class MobileNavbar {
   video.style.display = 'none';
   
 });
+
+// Efeito quando descer a pagina
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+  }
+  );
+  const elements = document.querySelectorAll('.hidden');
+  elements.forEach((element) => myObserver.observe(element)); {
+  };
